94ffd5f22ab29581314d414cbf1c09e6fa477882
swayam25 <178068815+swayam25@users.noreply.github.com>
2026-04-24 23:36:30 +0530

    Add YouTube Music to socials

--- src/lib/data/socials.ts
+++ src/lib/data/socials.ts
@@ -1,6 +1,7 @@
 import type { Component } from "svelte";
 import SimpleIconsDiscord from "~icons/simple-icons/discord";
 import SimpleIconsGithub from "~icons/simple-icons/github";
+import SimpleIconsYoutubemusic from "~icons/simple-icons/youtubemusic";
 
 interface Social {
     [key: string]: {
@@ -17,6 +18,10 @@ const socials: Social = {
     Discord: {
         url: "https://discord.com/users/1272113262455951464",
         icon: SimpleIconsDiscord
+    },
+    "YouTube Music": {
+        url: "https://music.youtube.com/@swayam25_yt",
+        icon: SimpleIconsYoutubemusic
     }
 };
 
